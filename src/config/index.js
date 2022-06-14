f07ba9f79d16e902e7be0e3bc7d3341f073c7451
bukkysos <chukwuebukaanyanwusos@gmail.com>
2022-06-14 11:31:40 +0100

    Changed base url to production

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-export const BASE_URL = `http://164.92.179.237:7071/api/v1/dashboard/`;
-// export const BASE_URL = `https://v1.ibib.io:7070/api/v1/dashboard/`;
+// export const BASE_URL = `http://164.92.179.237:7071/api/v1/dashboard/`;
+export const BASE_URL = `https://v1.ibib.io:7070/api/v1/dashboard/`;
 export const VERIFICATION_BASE_URL = `https://v1.ibib.io:7070/api/v1/dashboard/`;
 export const VERIFICATION_SHEET_BASE_URL = `https://v1.ibib.io:7072/api/v1/`;
